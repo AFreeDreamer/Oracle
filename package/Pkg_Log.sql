7329f3b19a6d59f298a7eb22d4c0003bf392912b
AFreeDreamer <sorliran@163.com>
2018-04-24 11:06:35 +0800

    Update Pkg_Log.sql

--- package/Pkg_Log.sql
+++ package/Pkg_Log.sql
@@ -1,31 +1,60 @@
-CREATE OR REPLACE Package Body Pkg_Log As
+/*	版本日志
+	  Version：v.1.1.o
+	  Date：2018-04-24
+	  Release Notes：
+		1、log_id修改为Rawtohex(sys_guid()),数据类型varchar2(32)
+		2、err_text、error_backtrace修改为500
+		3、删除序列seq_log_id
+*/
 
-    /*
-    Create Table t_log(
-           log_id Integer primary key,
-           object_name Varchar2(100),
-           start_time Date,
-           end_time Date,
-           err_code Number,
-           err_text Varchar2(1000),
-           err_time date,
-           error_backtrace Varchar2(200)
-    );
-    Create Sequence seq_log_id
-    Start With 1
-    Maxvalue 9999999999
-    Increment By 1
-    Cache 50;
-	Version：v1.0
-    */
+--创建日志表
+CREATE TABLE T_LOG(
+	   LOG_ID VARCHAR2(32) PRIMARY KEY,
+	   OBJECT_NAME VARCHAR2(100),
+	   START_TIME DATE,
+	   END_TIME DATE,
+	   ERR_TIME DATE,
+	   ERR_CODE NUMBER,
+	   ERR_TEXT VARCHAR2(500),
+	   ERROR_BACKTRACE VARCHAR2(500)
+);
+
+--创建程序包头
+CREATE OR REPLACE Package Pkg_Log As
+
     Function Log_Start
     (
         In_Object_Name Varchar2
        ,In_Start_Time  Date Default Sysdate
-    ) Return Integer As
-        v_Log_Id Integer;
+    ) Return Varchar2;
+
+    Procedure Log_End
+    (
+        In_Log_Id   Varchar2
+       ,In_End_Time Date Default Sysdate
+    );
+
+    Procedure Log_Err
+    (
+        In_Log_Id   Varchar2
+       ,In_Err_Code Number Default Sqlcode
+       ,In_Err_Text Varchar2 Default Substr(Sqlerrm, 1, 500)
+       ,In_Err_Time Date Default Sysdate
+       ,in_error_backtrace Varchar2 Default substr(dbms_utility.format_error_backtrace,1,500)
+    );
+
+End Pkg_Log;
+
+--创建程序包体
+CREATE OR REPLACE Package Body Pkg_Log As
+    Function Log_Start
+    (
+        In_Object_Name Varchar2
+       ,In_Start_Time  Date Default Sysdate
+    ) Return Varchar2 As
+        v_Log_Id Varchar2(32);
     Begin
-        v_Log_Id := Seq_Log_Id.Nextval;
+        v_Log_Id := Rawtohex(sys_guid());
         Execute Immediate 'insert into t_log(log_id,object_name,start_time) values(:1,:2,:3)'
             Using v_Log_Id, In_Object_Name, In_Start_Time;
         Commit;
@@ -34,7 +63,7 @@ CREATE OR REPLACE Package Body Pkg_Log As
 
     Procedure Log_End
     (
-        In_Log_Id   Integer
+        In_Log_Id   Varchar2
        ,In_End_Time Date Default Sysdate
     ) As
     Begin
@@ -45,22 +74,26 @@ CREATE OR REPLACE Package Body Pkg_Log As
 
     Procedure Log_Err
     (
-        In_Log_Id   Integer
-       ,In_Err_Code Number
-       ,In_Err_Text Varchar2
+        In_Log_Id   Varchar2
+       ,In_Err_Code Number Default Sqlcode
+       ,In_Err_Text Varchar2 Default Substr(Sqlerrm, 1, 500)
        ,In_Err_Time Date Default Sysdate
-       ,in_error_backtrace Varchar2 Default dbms_utility.format_error_backtrace
+       ,in_error_backtrace Varchar2 Default substr(dbms_utility.format_error_backtrace,1,500)
     ) As
-        v_err_code Number;
-        v_Err_Text Varchar2(200);
-        v_error_backtrace Varchar2(200);
+        --v_err_code Number;
+        --v_Err_Text Varchar2(200);
+        --v_error_backtrace Varchar2(200);
     Begin
-        v_err_code := In_Err_Code;
-        v_Err_Text := Substr(In_Err_Text, 1, 200);
-        v_error_backtrace := Substr(in_error_backtrace, 1, 200);
+        --v_err_code := In_Err_Code;
+        --v_Err_Text := Substr(In_Err_Text, 1, 200);
+        --v_error_backtrace := Substr(in_error_backtrace, 1, 200);
         Execute Immediate 'update t_log set err_code=:1,err_text=:2,err_time=:3,error_backtrace=:4 where log_id=:5'
-            Using v_err_code, v_Err_Text, In_Err_Time,v_error_backtrace,In_Log_Id;
+            Using In_Err_Code, In_Err_Text, In_Err_Time,in_error_backtrace,In_Log_Id;
         Commit;
     End Log_Err;
 
 End Pkg_Log;
+
+
+
+
